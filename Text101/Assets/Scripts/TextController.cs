5c1895b64a3ac5861cc5a15c3551a4cf7e5ade3d
MarkHeu <Mark.Heuser@outlook.de>
2017-04-23 09:29:41 +0200

    Session 3 Lektion 30
    Nach Lektion 30

---
+++ Text101/Assets/Scripts/TextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class TextController : MonoBehaviour {
+	public Text text; 
+	// Use this for initialization
+	void Start () 
+	{
+		text.text = "Spiel gestartet"; 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			text.text = "Pfeiltaste runter gedrueckt"; 
+		}
+	}
+}
