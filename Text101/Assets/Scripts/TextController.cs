41e2c0a527e45a3ac5cdff49e42a91b908e0e29e
MarkHeu <Mark.Heuser@outlook.de>
2017-04-24 07:15:21 +0200

    Lection ready

--- Text101/Assets/Scripts/TextController.cs
+++ Text101/Assets/Scripts/TextController.cs
@@ -4,7 +4,17 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour {
 	public Text text; 
-	private enum Status {Zelle, Spiegel, Zellenspiegel, Tuerschloss_auf, Tuerschloss_zu, Laken_1, Laken_2, Freiheit}
+	private enum Status 
+	{	
+		Zelle, 
+		Spiegel, 
+		Zellenspiegel, 
+		Tuerschloss_auf, 
+		Tuerschloss_zu, 
+		Laken_1, 
+		Laken_2, 
+		Freiheit
+	}
 	private Status meinStatus; 
 	// Use this for initialization
 	void Start () 
@@ -16,23 +26,14 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (meinStatus == Status.Zelle) {
-			StatusZelle (); 
-		} else if (meinStatus == Status.Laken_1) {
-			StatusLaken1 ();
-		} else if (meinStatus == Status.Tuerschloss_zu) {
-			StatusTuerschlosszu (); 
-		} else if (meinStatus == Status.Spiegel) {
-			StatusSpiegel (); 
-		} else if (meinStatus == Status.Zellenspiegel) {
-			StatusZellenSpiegel (); 
-		} else if (meinStatus == Status.Laken_2) {
-			StatusLaken2 ();
-		} else if (meinStatus == Status.Tuerschloss_auf) {
-			StatusTuerschlossauf ();
-		} else if (meinStatus == Status.Freiheit) {
-			StatusFreiheit ();
-		}
+		if 		(meinStatus == Status.Zelle) 			{StatusZelle();} 
+		else if (meinStatus == Status.Laken_1) 			{StatusLaken1();} 
+		else if (meinStatus == Status.Tuerschloss_zu) 	{StatusTuerschlosszu();} 
+		else if (meinStatus == Status.Spiegel) 			{StatusSpiegel();} 
+		else if (meinStatus == Status.Zellenspiegel) 	{StatusZellenSpiegel();} 
+		else if (meinStatus == Status.Laken_2) 			{StatusLaken2();} 
+		else if (meinStatus == Status.Tuerschloss_auf) 	{StatusTuerschlossauf();} 
+		else if (meinStatus == Status.Freiheit) 		{StatusFreiheit();}
 	}
 	void StatusZelle()
 	{
@@ -42,70 +43,43 @@ public class TextController : MonoBehaviour {
 					"Drücke L um das Laken zu betrachen!\n" +
 					"Drücke S um den Spiegel zu betrachen!\n" +
 					"Drücke T um das Türschloß zu betrachen!";
-		if (Input.GetKeyDown (KeyCode.L)) 
-		{
-			meinStatus = Status.Laken_1;
-		}
-		else if (Input.GetKeyDown (KeyCode.T)) 
-		{
-			meinStatus = Status.Tuerschloss_zu;
-		}
-		else if (Input.GetKeyDown (KeyCode.S)) 
-		{
-			meinStatus = Status.Spiegel;
-		}
+		if 		(Input.GetKeyDown (KeyCode.L)) {meinStatus = Status.Laken_1;}
+		else if (Input.GetKeyDown (KeyCode.T)) {meinStatus = Status.Tuerschloss_zu;}
+		else if (Input.GetKeyDown (KeyCode.S)) {meinStatus = Status.Spiegel;}
 	}
 	void StatusLaken1()
 	{
 		text.text = "Diese Laken sind ekelhaft! \n\n" + 
 					" Drücke R um zurück zu gelangen!";
-		if (Input.GetKeyDown (KeyCode.R)) 
-		{
-			meinStatus = Status.Zelle;
-		}
+		if (Input.GetKeyDown (KeyCode.R)) {meinStatus = Status.Zelle;}
 	}
 	void StatusLaken2()
 	{
 		text.text = "Diese Laken sind noch immer ekelhaft! \n\n" + 
 			" Drücke R um zurück zu gelangen!";
-		if (Input.GetKeyDown (KeyCode.R)) 
-		{
-			meinStatus = Status.Zellenspiegel;
-		}
+		if (Input.GetKeyDown (KeyCode.R)) {meinStatus = Status.Zellenspiegel;}
 	}
 	void StatusTuerschlosszu()
 	{
 		text.text = "Diese Tür ist verschlossen! \n\n" + 
 			" Drücke R um zurück zu gelangen!";
-		if (Input.GetKeyDown (KeyCode.R)) 
-		{
-			meinStatus = Status.Zelle;
-		}
+		if (Input.GetKeyDown (KeyCode.R)) {meinStatus = Status.Zelle;}
 	}
 	void StatusTuerschlossauf()
 	{
 		text.text = "Diese Tür ist verschlossen!\n" +
 					" aber der Schlüssel steckt!\n\n" + 
 					" Drücke O um ihn zu drehen!";
-		if (Input.GetKeyDown (KeyCode.O)) 
-		{
-			meinStatus = Status.Freiheit;
-		}
+		if (Input.GetKeyDown (KeyCode.O)) {meinStatus = Status.Freiheit;}
 	}
 	void StatusSpiegel()
 	{
 		text.text = "Dieser Spiegel ist locker! \n\n" + 
 			" Drücke N um den Spiegel zu nehmen!\n" +
 			" Drücke R um zurück zu gelangen!";
-		if (Input.GetKeyDown (KeyCode.R)) 
-		{
-			meinStatus = Status.Zelle;
-		}
-		else if (Input.GetKeyDown(KeyCode.N))
-		{
-			meinStatus = Status.Zellenspiegel;
-			print("Spiegel genommen");
-		}
+		if 		(Input.GetKeyDown (KeyCode.R)) 	{meinStatus = Status.Zelle;}
+		else if (Input.GetKeyDown(KeyCode.N))	{meinStatus = Status.Zellenspiegel;
+												print("Spiegel genommen");}
 	}
 	void StatusZellenSpiegel()
 	{
