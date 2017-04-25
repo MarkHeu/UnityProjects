f0ed02fceeb5d20df10b0950ad940c18064e935e
MarkHeu <Mark.Heuser@outlook.de>
2017-04-25 11:45:22 +0200

    Lektion 5: Block Breaker

---
+++ NumberWizardUI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+ public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        // old
+        // Application.LoadLevel(name);
+        // new
+        SceneManager.LoadScene(name); 
+    }
+ public void Quit()
+    {
+        Debug.Log("Quit Game Requested");
+        Application.Quit();
+    }
+}
---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour 
+{
+	private int max ; 
+	private int min; 
+	private int guess;
+    private int maxguessesallowed = 10;
+    public Text text;
+	// Use this for initialization
+	void Start () 
+	{
+		StartGame (); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+     
+	}
+	void StartGame()
+	{	
+		max = 1000; 
+		min = 1;
+        guess = Random.Range(min, max);
+        max = max+1;
+        text.text = guess.ToString();
+
+    }
+	void NextGuess()
+	{	
+        guess = Random.Range(min, max);
+        text.text = guess.ToString(); 
+        maxguessesallowed = maxguessesallowed - 1; 
+        if (maxguessesallowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+	}
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void GuessLower()
+    {
+        max = guess; 
+        NextGuess();
+    }
+    public void GuessRight()
+    {
+        SceneManager.LoadScene("Lose");
+
+    }
+}
