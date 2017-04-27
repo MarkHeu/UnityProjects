55886cff28df6a74fdf64b9ace9e844b970973df
MarkHeu <Mark.Heuser@outlook.de>
2017-04-27 18:02:29 +0200

    BlockBreaker 32/45

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -3,31 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-    public int maxHits;
+
     private int timesHit;
-    private LevelManager levelManager; 
-	// Use this for initialization
+    private LevelManager levelManager;
+    public Sprite[] hitSprites;
+
+
 	void Start ()
     {
         timesHit = 0;
         levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        timesHit = timesHit + 1; 
+        bool isBreakable = this.tag == "Breakable";
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+    }
+    void HandleHits()
+    {
+        timesHit = timesHit + 1;
+        int maxHits = hitSprites.Length + 1;
         if (timesHit == maxHits)
         {
             Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+
         }
     }
     void WintheLevel()
     {
         levelManager.LoadNextLevel(); 
+    }
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
     }
 }
