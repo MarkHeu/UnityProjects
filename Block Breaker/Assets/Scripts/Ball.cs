40432a797bb005c622be1807a9ca5037b479d90d
MarkHeu <Mark.Heuser@outlook.de>
2017-04-27 15:34:10 +0200

    Lektion 80/81

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private Vector3 paddletoBallVector;
+    private Rigidbody2D rb;
+    private bool hasStarted = false; 
+    // Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        paddletoBallVector = this.transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            this.transform.position = paddle.transform.position + paddletoBallVector;
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true; 
+                rb.velocity = new Vector2(2f, 10f); //Launch the Ball 
+            }
+        }
+	}
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    private float mousepos;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f); 
+
+        mousepos = (Input.mousePosition.x /Screen.width * 16);
+        paddlePos.x = Mathf.Clamp(mousepos, 0.5f, 15.5f); 
+        this.transform.position = paddlePos; 
+        
+	}
+}
